e68795dc655b2668f1aa571f67849b90483dbb86
Jon Calhoun <joncalhoun@gmail.com>
2018-05-06 13:48:24 -0400

    wrote code to get all retweeters usernames

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"io"
+	"fmt"
 	"net/http"
 	"os"
 	"strings"
@@ -49,10 +49,34 @@ func main() {
 
 	var conf oauth2.Config
 	tclient := conf.Client(context.Background(), &token)
-	res2, err := tclient.Get("https://api.twitter.com/1.1/statuses/retweets/991053593250758658.json")
+	usernames, err := retweeters(tclient, "991053593250758658")
 	if err != nil {
 		panic(err)
 	}
-	defer res2.Body.Close()
-	io.Copy(os.Stdout, res2.Body)
+	fmt.Println(usernames)
+}
+
+func retweeters(client *http.Client, tweetID string) ([]string, error) {
+	url := fmt.Sprintf("https://api.twitter.com/1.1/statuses/retweets/%s.json", tweetID)
+	res, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var retweets []struct {
+		User struct {
+			ScreenName string `json:"screen_name"`
+		} `json:"user"`
+	}
+	dec := json.NewDecoder(res.Body)
+	err = dec.Decode(&retweets)
+	if err != nil {
+		return nil, err
+	}
+	usernames := make([]string, 0, len(retweets))
+	for _, retweet := range retweets {
+		usernames = append(usernames, retweet.User.ScreenName)
+	}
+	return usernames, nil
 }
