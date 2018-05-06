fb714c2208bd03dc7511a20544add645ae68298d
Jon Calhoun <joncalhoun@gmail.com>
2018-05-06 13:35:11 -0400

    Finished authenticating

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"strings"
+
+	"golang.org/x/oauth2"
 )
 
 func main() {
@@ -18,5 +23,36 @@ func main() {
 	defer f.Close()
 	dec := json.NewDecoder(f)
 	dec.Decode(&keys)
-	fmt.Printf("%+v\n", keys)
+	// fmt.Printf("%+v\n", keys)
+
+	req, err := http.NewRequest("POST", "https://api.twitter.com/oauth2/token", strings.NewReader("grant_type=client_credentials"))
+	if err != nil {
+		panic(err)
+	}
+	req.SetBasicAuth(keys.Key, keys.Secret)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+
+	var client http.Client
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	var token oauth2.Token
+	dec = json.NewDecoder(res.Body)
+	err = dec.Decode(&token)
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Println(token)
+
+	var conf oauth2.Config
+	tclient := conf.Client(context.Background(), &token)
+	res2, err := tclient.Get("https://api.twitter.com/1.1/statuses/retweets/991053593250758658.json")
+	if err != nil {
+		panic(err)
+	}
+	defer res2.Body.Close()
+	io.Copy(os.Stdout, res2.Body)
 }
